4edcdef4a740decb8d280de87bf4209fd3f9ba82
Tim Gremp <timothy.gremp@gmail.com>
2024-10-19 04:58:53 -0600

    initial commit to save cursor work so far

---
+++ MeditateMe/MeditateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditateMeApp.swift
+//  MeditateMe
+//
+//  Created by Alaryce Patterson on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MeditateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
