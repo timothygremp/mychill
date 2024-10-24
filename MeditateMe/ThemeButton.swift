0d784587fe55175853e7b429c39318883cb7283c
Alaryce Patterson <tim@gmail.com>
2024-10-24 14:51:01 -0600

    add gradient selection color to themes

--- MeditateMe/ThemeButton.swift
+++ MeditateMe/ThemeButton.swift
@@ -8,24 +8,22 @@ struct ThemeButton: View {
     var body: some View {
         Button(action: action) {
             Text(theme)
-                .font(.headline)
-                .padding()
-                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 12)
+                .padding(.vertical, 6)
                 .background(
                     ZStack {
                         if isSelected {
                             LinearGradient(gradient: Gradient(colors: [Color(hex: "#FFB347"), Color(hex: "#FF69B4")]), startPoint: .leading, endPoint: .trailing)
-                                .cornerRadius(20)
                         } else {
                             Color.white.opacity(0.2)
-                                .cornerRadius(20)
                         }
                     }
                 )
-                .foregroundColor(isSelected ? .white : .black)
+                .foregroundColor(.white) // This makes the text white
+                .cornerRadius(15)
                 .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(isSelected ? Color.clear : Color.gray, lineWidth: 1)
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.white, lineWidth: isSelected ? 0 : 1)
                 )
         }
         .buttonStyle(PlainButtonStyle()) // Ensure no default button style is applied
